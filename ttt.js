c3d5afa9b5f07cbc2dc2d0d4a07cfef5de16e285
Aodj05 <aodj05@msn.com>
2022-03-14 13:26:27 -0400

    setup html

---
+++ ttt.js
@@ -0,0 +1,19 @@
+//init board into an array
+
+//store players into object
+
+//make function to add mark
+
+//tie function to DOM
+
+// build game object
+
+//game over check
+
+// name input
+
+// start/restart buttons
+
+//results message
+
+// game AI
